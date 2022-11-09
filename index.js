ec0b092bbb22e099a06a83f8ff942f75086b3a6e
Ilya <ilya.shuvatov1@gmail.com>
2022-11-09 11:17:03 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let existingUserLogin = 'the_best_user';
+let existingUserPassword = '12345678';
+
+let userLogin = prompt('Введите логин').trim();
+let userPassword = prompt('Введите пароль').trim();
+
+if (existingUserLogin === userLogin && existingUserPassword === userPassword) {
+    alert(`Добро пожаловать ${userLogin}`)
+} else {
+    alert('Логин и(или) пароль введены неверно!')
+}
+
